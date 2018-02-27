40ed15963c7cb6060905c564a99258cc5c453f4e
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-02-27 14:16:35 +0100

    upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExRemoteDockers.MixProject do
   def project do
     [
       app: :ex_remote_dockers,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
