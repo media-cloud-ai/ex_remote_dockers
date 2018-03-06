04fe7f10ff25a8590ef00ac6a565862f006e0638
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2018-03-06 17:58:07 +0100

    revert test configuration

--- config/test.exs
+++ config/test.exs
@@ -3,5 +3,5 @@ use Mix.Config
 config :remote_dockers,
   hostname: "https://192.168.99.100",
   port: 2376,
-      certfile: "/Users/marco/.docker/machine/certs/cert.pem",
-      keyfile: "/Users/marco/.docker/machine/certs/key.pem"
+      certfile: "/path/to/cert.pem",
+      keyfile: "/path/to/key.pem"
